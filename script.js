7bed968dbb6685c76763e2e1569283038395cfba
rrmmss86 <rainerschoen86@gmail.com>
2025-09-24 15:01:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import * as THREE from './three.module.js';
-import { GLTFLoader } from './GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 
 const container = document.getElementById('canvas-container');
 
