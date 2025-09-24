32a20cdbf6aa1303658329594a1fc03554c6aac3
rrmmss86 <rainerschoen86@gmail.com>
2025-09-24 15:42:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x111111);
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
 camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
