2631daee5033d00ae56ff98e4f8e7cecbeb6b728
rrmmss86 <rainerschoen86@gmail.com>
2025-09-24 16:24:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
 import { GLTFLoader } from './libs/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from './libs/OrbitControls.js';
 
 const container = document.getElementById('canvas-container');
 
